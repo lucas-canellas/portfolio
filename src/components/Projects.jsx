f0001f42d701dcd9f2dea787fea58ebd0792143e
Lucas David <lucascanellasdev@gmail.com>
2023-10-17 12:08:17 -0300

    att display card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ export const Projects = () => {
                     <h2 className="text-cinder dark:text-text-light text-3xl font-bold font-plus">Projetos pessoais </h2>
                 </div>
                 <div className="">
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-full">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 w-full">
                         {
                             data && data.map(project => <Card github={project.attributes.github} image={project.attributes.image} preview={project.attributes.preview} stack={project.attributes.stack} subtitle={project.attributes.subtitle} title={project.attributes.title} key={project.attributes.title} />)
                         }
